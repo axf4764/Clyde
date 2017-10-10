dfd44d3ae6775acf4eb9fc7afcc7b941046f754e
JamesFriedenberg <jpf9146@g.rit.edu>
2017-10-10 15:02:02 -0400

    Started Scripting for googas and player
    Using bools to check if the integer ID on the weapon matches the one on
    the googa. If it does, destroy the googa's gameObject

---
+++ Clyde Project/Assets/Scripts/GoogaParent.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoogaParent : MonoBehaviour
+{
+    public int shotCheck;
+    public Player killer = new Player();
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    bool CorrectShot(int checker)
+    {
+        if(checker == killer.shotType)
+        {
+            Destroy(gameObject);
+            return true;
+        }
+        return false;
+    }
+}
