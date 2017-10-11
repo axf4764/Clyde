4316eb95bec7aacca4933f91e580ead7fde13851
Corinne <cmg7063@g.rit.edu>
2017-10-11 13:17:38 -0400

    player class
    begin adding basic movement structure to player class

--- Clyde Project/Assets/Scripts/Player.cs
+++ Clyde Project/Assets/Scripts/Player.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-    public int shotType;
+    // Types of weaponry
+    public GameObject bow;
+    public GameObject spear;
+    public GameObject hatchet;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    private Vector3 position;
+    private Vector3 velocity;
+    private Vector3 direction;
+    private Vector3 acceleration;
+
+    // These two attributes  will prevent bullet rapid fire 
+    private float fireDelay;
+    private float fireTimer;
+
+    // Use this for initialization
+    void Start () {
+        GameObject sceneManagerObj = GameObject.FindWithTag("GameController");
+
+        position = new Vector3(0, 0, 0);
+        velocity = new Vector3(0, 0, 0);
+        direction = new Vector3(0, 1, 0);
+        acceleration = new Vector3(0, 0, 0);
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        fireTimer -= Time.deltaTime;
+    }
 }
