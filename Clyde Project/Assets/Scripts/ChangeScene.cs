d8b001952b8ea1ccaeded8d1cabcc8363726798a
cmg7063 <cmg7063@g.rit.edu>
2017-10-12 02:28:03 -0400

    set up scene HUD and stuff
    text isn't showing in camera help

--- Clyde Project/Assets/Scripts/ChangeScene.cs
+++ Clyde Project/Assets/Scripts/ChangeScene.cs
@@ -1,17 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 //Benjamin
 public class ChangeScene : MonoBehaviour
 {
+    // Attributes
+    public Button start;
+    public Button options;
 
     public string TargetSceneName;
     // Use this for initialization
     void Start()
     {
 
+    }
+
+    public void Play()
+    {
+        Application.LoadLevel("Scene1");
+    }
+
+    public void Options()
+    {
+        Application.LoadLevel("Options");
     }
 
     // Update is called once per frame
@@ -23,7 +37,7 @@ public class ChangeScene : MonoBehaviour
     {
         if( collision.gameObject.tag == "Player" )
         {
-            SceneManager.LoadScene( TargetSceneName );
+           // SceneManager.LoadScene( TargetSceneName );
         }
     }
 }
