5b0708c9b0c3e4edfa0a4c195b712f863ca496c3
Zanthous <bcm023@gmail.com>
2017-10-26 02:32:53 -0400

    Cutscenes
    I added some placeholder cutscenes, a lot of dialogue and started on gamemanager. The player has to complete the wood cutting minigame first now, but there are still a couple bugs I have to fix. I got the very start of the stealth game in too. (Benjamin)

---
+++ Clyde Project/Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManagerScript : MonoBehaviour
+{
+    public static GameManagerScript instance = null;
+
+    [Header("Variables")]
+    private bool hasWood = false;
+
+    //todo add language variable
+
+    public bool HasWood
+    {
+        get;
+        set;
+    }
+    void Awake()
+    {
+        if( instance == null )
+        { 
+            instance = this;
+        }
+        else if( instance != this )
+        { 
+            Destroy( gameObject );
+        }
+        DontDestroyOnLoad( gameObject );
+
+        InitializeVariables();
+    }
+
+    // Use this for initialization
+    void InitializeVariables()
+    {
+        //add stuff as needed here
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Clyde Project/Assets/Scripts/Overworld/PlayerMovement.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Benjamin
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = .02f;
+    private Rigidbody2D body;
+    // Use this for initialization
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void DoMovement()
+    {
+        Vector2 moveVector = Vector2.zero;
+        //basics
+        if( Input.GetKey( KeyCode.W ) )
+        {
+            moveVector.y += speed;
+        }
+        if( Input.GetKey( KeyCode.A ) )
+        {
+            moveVector.x -= speed;
+        }
+        if( Input.GetKey( KeyCode.S ) )
+        {
+            moveVector.y -= speed;
+        }
+        if( Input.GetKey( KeyCode.D ) )
+        {
+            moveVector.x += speed;
+        }
+        if (moveVector!= Vector2.zero)
+            moveVector = moveVector.normalized * speed;
+
+        //hard coded stuff | makes the character smaller as he moves upward
+        float yLimit = -.7f;
+        transform.localScale = new Vector3(
+            1.75f - Mathf.Abs( yLimit - this.transform.position.y ),
+            1.75f - Mathf.Abs( yLimit - this.transform.position.y ),
+            1.75f - Mathf.Abs( yLimit - this.transform.position.y )
+            ); 
+
+        body.MovePosition( new Vector2( transform.position.x + moveVector.x, transform.position.y + moveVector.y ) );
+
+    }
+}
