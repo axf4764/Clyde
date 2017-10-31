7e27e8bd34cd4ac1780f09c61034c3bd3c9af5ec
cmg7063 <cmg7063@g.rit.edu>
2017-10-31 04:03:36 -0400

    minigame basic fxns
    watch repair minigame and cutscene leading to minigame. main menu & controls menu completed; options menu needs options

---
+++ Clyde Project/Assets/Scripts/Type Master/Enemy.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour
+{
+
+    float fireprogress = 0.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector2.Distance(this.transform.position, Vector2.zero) > 1.0f)
+        {
+            this.transform.LookAt(Vector2.zero);
+            this.transform.position = Vector2.MoveTowards(this.transform.position, Vector2.zero, 0.01f);
+        }
+        else
+        {
+            fireprogress += Time.deltaTime;
+            if (fireprogress > 1.50f)
+            {
+                Debug.Log("fire");
+                fireprogress -= 1.50f;
+            }
+        }
+    }
+}
