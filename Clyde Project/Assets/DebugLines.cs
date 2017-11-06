3727210d1a4222c6a11aae6b07a660a6a4493794
Zanthous <bcm023@gmail.com>
2017-11-05 22:40:51 -0500

    Stealth and bug fixes
    .

---
+++ Clyde Project/Assets/DebugLines.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugLines : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    //void OnTriggerStay( Collision collision )
+    //{
+    //    foreach( ContactPoint contact in collision.contacts )
+    //    {
+    //        //print( contact.thisCollider.name + " hit " + contact.otherCollider.name );
+    //        Debug.DrawRay( contact.point, contact.normal, Color.white );
+    //    }
+    //}
+    private void OnTriggerStay2D( Collider2D collision )
+    {
+
+        //foreach( ContactPoint2D contact in  )
+        //{
+        //    //print( contact.thisCollider.name + " hit " + contact.otherCollider.name );
+        //    Debug.DrawRay( contact.point, contact.normal, Color.white );
+        //}
+    }
+}
---
+++ Clyde Project/Assets/SendBack.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SendBack : MonoBehaviour {
+
+    public GameObject Player;
+    public Transform Respawn;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D( Collider2D collision )
+    {
+        if(collision.tag == "Player")
+        {
+            Player.transform.position = Respawn.position;
+        }
+
+    }
+
+}
